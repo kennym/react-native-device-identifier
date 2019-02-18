24920a62bdde662d2b0c559dbd78c48be55a9d27
Kenny Meyer <kenny@kennymeyer.net>
2019-02-18 16:17:17 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+import React, {
+  PixelRatio,
+  Platform,
+  Dimensions
+} from 'react-native';
+
+const windowDimensions = Dimensions.get('window');
+
+class ReactNativeDeviceIdentifier {
+  constructor() {
+    this.width = windowDimensions.width
+    this.height = windowDimensions.height
+  }
+
+  /**
+   * Returns iPhone model name as a string.
+   * 
+   * Return values:
+   * - iPhone SE
+   * 
+   * TODO:
+   * - iPhone X
+   * - iPhone 7/8
+   * - iPhone XS
+   */
+  getModelName() {
+    if (this.isIOS() && this.height === 1136 && this.width === 640) {
+      return "iPhone SE"
+    } else {
+      return "Not implemented"
+    }
+  }
+  
+  isIOS() {
+    if (Platform.OS === 'ios') return true
+  }
+
+  isAndroid() {
+    if (Platform.OS === 'android') return true
+  }
+}
+
+module.exports = new ReactNativeDeviceIdentifier();
